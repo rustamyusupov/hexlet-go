e55782566bb17db2929576319d818638e432b655
Rustam Yusupov <i@rstm.me>
2024-08-01 11:05:24 +0200

    Update main.go to handle HTTP requests and add routes for different pages

--- main.go
+++ main.go
@@ -1,12 +1,36 @@
 package main
 
 import (
-	"fmt"
-	"go-hello/greeting"
-
-	"github.com/fatih/color"
+	"net/http"
 )
 
 func main() {
-	fmt.Println(color.HiYellowString(greeting.Get()))
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome to Hexlet"))
+	})
+
+	http.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hexlet is the leading educational platform for Software Engineers"))
+	})
+
+	http.HandleFunc("/courses", func(w http.ResponseWriter, r *http.Request) {
+		page := r.URL.Query().Get("page")
+		var pageCourses string
+
+		switch page {
+		case "":
+			pageCourses = "Введите номер курса!"
+		case "1":
+			pageCourses = "Как написать свой первый \"Hello world\" на go..."
+		case "2":
+			pageCourses = "Как работает сборщик мусора в go..."
+		default:
+			pageCourses = "Курс в разработке..."
+		}
+
+		// возвращаем страницу курса
+		w.Write([]byte(pageCourses))
+	})
+
+	http.ListenAndServe(":80", nil)
 }
